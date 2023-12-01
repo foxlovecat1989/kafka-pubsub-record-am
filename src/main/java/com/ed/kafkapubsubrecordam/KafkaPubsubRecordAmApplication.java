d6c0c4f8e6e1ed62859045ba3d44f3b227849cc2
foxlovecatii <foxlovecat1989@gmail.com>
2023-12-01 18:22:11 +0800

    init project

---
+++ src/main/java/com/ed/kafkapubsubrecordam/KafkaPubsubRecordAmApplication.java
@@ -0,0 +1,13 @@
+package com.ed.kafkapubsubrecordam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPubsubRecordAmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPubsubRecordAmApplication.class, args);
+    }
+
+}
